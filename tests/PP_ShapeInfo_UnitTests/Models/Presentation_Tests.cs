260a4da36bbc1134ebbb1377dc8f6b30c360b9cb
Raffael Dieske <r.dieske@outlook.de>
2021-08-25 16:36:26 +0200

    Move tests to designated folder

---
+++ tests/PP_ShapeInfo_UnitTests/Models/Presentation_Tests.cs
@@ -0,0 +1,17 @@
+﻿using PP_ShapeInfo.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace PP_ShapeInfo_UnitTests.Models
+{
+    public class Presentation_Tests
+    {
+        [Fact]
+        public void Presentation_PassSlids_ShouldConstructWithSlides()
+        {
+            var sut = new Slide(DummyData.SlideShapes);
+
+            Assert.Equal(DummyData.SlideShapes, sut.SlideShapes);
+        }
+    }
+}
